8b265dbb598528d51836ef4328b591b383fd46f5
MiguelAngelH <miguel.angelhm920@gmail.com>
2025-06-18 16:22:54 -0400

    No code changes made.

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 # Base para conectar la API de Gemini desde Python
 import requests
 
-GEMINI_API_KEY = "AIzaSyBv--bdLrk0p0fuCmd3fBK3fp6CogJ8iQs"  # Reemplaza con tu clave real
+GEMINI_API_KEY = "TU-API"  # Reemplaza con tu clave real
 GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + GEMINI_API_KEY
 
 
